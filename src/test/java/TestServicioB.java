85d2682453eccbe9aa285fb53e982e09f0851b8a
osama <osamakhtar@gmail.com>
2018-08-27 13:11:46 +0200

    Second Commit

--- src/test/java/TestServicioB.java
+++ src/test/java/TestServicioB.java
@@ -1,6 +1,8 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.arquitecturajava.servicios.ServicioA;
+import com.arquitecturajava.servicios.ServicioAImpl;
 import com.arquitecturajava.servicios.ServicioB;
 import com.arquitecturajava.servicios.ServicioBImpl;
 
@@ -13,5 +15,18 @@ public class TestServicioB {
 		 Assert.assertEquals(servicioB.multiplicar(2, 3),6);
 		  
 	}
+	
+	@Test
+	 public void testmultiplicarSumar() {
+	  
+	 ServicioA servicioA=new ServicioAImpl();
+	  
+	 ServicioB servicioB= new ServicioBImpl();
+	  
+	  
+	 servicioB.setServicioA(servicioA);
+	 Assert.assertEquals(servicioB.multiplicarSumar(2, 3, 2),10);
+	  
+	 }
 
 }
